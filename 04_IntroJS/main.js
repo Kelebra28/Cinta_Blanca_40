b45e6477a331879ef311b8736c381997905b93a7
Kelebra <ricardokelebra@gmail.com>
2020-05-04 21:32:36 -0500

    intro javaScript

---
+++ 04_IntroJS/main.js
@@ -0,0 +1,51 @@
+//alert('Ganaste un Iphone, Dale Click!!!');
+
+
+// VARIABLES------->
+// Estructura
+// var nombre  = valor
+
+//Valores------>
+//Numerico
+var numero = 10;
+//String o texto
+var perrito = 'Misha';
+//Boolean
+var verdadero = false; //true
+
+// Especiales---->
+//object
+var objecto = {};
+//Array
+var arreglo = [];
+
+
+var num1 = 500;
+var num2 = 100;
+
+// ----Salidas o outputs---
+
+var suma = num1 + num2;
+console.log(suma);
+
+var nombre = 'Ricardo';
+var apellido = 'Basurto';
+
+var nombreCompleto = nombre + ' ' + apellido;
+
+console.log(nombreCompleto);
+
+// ----Entradas o inputs----
+
+var pregunta =  prompt('Ingresa tu nombre');
+
+console.log(pregunta);
+
+//Challenge
+
+//       1
+// Van a preguntar el nombre, direccion, telefono al usuario
+// Y mostrarlo en la consola
+
+//       2
+//Preguntar el anio en que nacio y mostrar en la consola que edad deberia de tener este anio
