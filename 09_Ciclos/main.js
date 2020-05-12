973f95906bb9e22c82f89fa89e632a0d5bf32ce0
Kelebra <ricardokelebra@gmail.com>
2020-05-11 21:44:04 -0500

    cilos-funciones

---
+++ 09_Ciclos/main.js
@@ -0,0 +1,33 @@
+// Vanilla JS
+// var
+
+//ECMA 5
+// let 
+
+// for(let i = 0 ; i <= 100 ; i = i + 3 ){
+//     console.log(i)
+// }
+
+
+// 50-500 -> 25
+//100 - 0 -> -5
+//200 - 600 -> 4
+
+let animales = ["perrito", "mishi", "cerdito", "paloma", "pulpo", "perrito", "mishi", "cerdito", "paloma", "pulpo", "hola"]
+
+// console.log(animales.length) //--10
+
+for(let x = 0; x < animales.length; x++){
+
+    console.log(animales[x])
+}
+
+let palabra = "Asdgiuyasiofjojuhdsfgp"
+
+// console.log(palabra.length)
+
+//Challenge ---> FizzBuzz
+
+
+
+ 
---
+++ 10_Funciones/main.js
@@ -0,0 +1,28 @@
+
+
+function saludo (){
+    console.log("Hola!")
+}
+
+// saludo()
+
+// let num1 = 10
+// let num2 = 50
+
+// let suma = num1 + num2
+
+function suma (a, b){
+    return a + b
+}
+
+console.log(suma(2 ,2))
+
+console.log(suma(suma(10, 10), 50))
+console.log(suma("c ", "x"))
+
+
+// function suma2 (a , b , c) {
+//     console.log(a +  b * c)
+// }
+
+// suma2(10 , 20 , 5)
